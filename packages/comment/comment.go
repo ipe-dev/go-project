3fc61b5159e6fbfcdc3273eaf3b31db91bc5df1e
ipe <izaki.yb1998@gmail.com>
2021-04-29 12:26:59 +0900

    createdate追加

--- packages/comment/comment.go
+++ packages/comment/comment.go
@@ -1,10 +1,11 @@
 package comment
 
 type Comment struct {
-	Id       int
-	PostId   int
-	UserName string
-	Content  string
+	Id         int
+	PostId     int
+	UserId     int
+	Content    string
+	CreateDate string
 }
 
 func create() {
